4b919e3bd3fa972f018f248957bca5d2a0630a77
Xavier Pessoles <xpessoles.ptsi@free.fr>
2025-02-04 20:48:01 +0100

    MAJ

--- 16_RC4/Modeles/Modele_causal_Simulink/parametres.m
+++ 16_RC4/Modeles/Modele_causal_Simulink/parametres.m
@@ -35,4 +35,4 @@ ISAT = 4;   % Courant de saturation (4A ou 2A).
 
 %% Paremètres cinématiques
 vmax = 0.5;   %m/s : vitesse du trapèze
-amax =  5;   %m/s² : accélération du trapèze
+amax = 5;   %m/s² : accélération du trapèze
